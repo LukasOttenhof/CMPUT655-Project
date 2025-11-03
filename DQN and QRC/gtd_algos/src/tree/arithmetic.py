1eeca19cf28de2629d37f7837f09815d7c98024f
Punreach Rany <punreach.rany@yahoo.com>
2025-11-03 10:57:12 -0700

    [feat]: added QRC algorithm into the code

---
+++ DQN and QRC/gtd_algos/src/tree/arithmetic.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+from jax.tree_util import tree_map
+from jax import vmap
+import jax.numpy as jnp
+
+
+def add(*args):
+    return reduce(lambda a, b: tree_map(jnp.add, a, b), args)
+
+
+def subtract(*args):
+    return reduce(lambda a, b: tree_map(jnp.subtract, a, b), args)
+
+
+def scale(scalar, arg):
+    return tree_map(lambda x: scalar * x, arg)
+
+
+def vmap_scale(vector, arg):
+    f = vmap(scale, in_axes=[0, 0])
+    return f(vector, arg)
+
+
+def neg(arg):
+    return scale(-1.0, arg)
+
+
+def zeros(arg):
+    return scale(0.0, arg)
